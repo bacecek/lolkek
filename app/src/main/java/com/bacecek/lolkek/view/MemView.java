0899bf37a733b9d8dfa0e0c722e2242275c3319d
chichkanov <agchichkanov@gmail.com>
2017-08-12 14:56:43 +0300

    gigigi

--- app/src/main/java/com/bacecek/lolkek/view/MemView.java
+++ app/src/main/java/com/bacecek/lolkek/view/MemView.java
@@ -1,7 +1,13 @@
 package com.bacecek.lolkek.view;
 
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+import com.bacecek.lolkek.data.ScreenState;
+
 /**
  *
  */
 public interface MemView extends BaseView {
+    @StateStrategyType(AddToEndSingleStrategy.class)
+    void showScreenState(ScreenState screenState);
 }
